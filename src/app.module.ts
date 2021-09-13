44e8e5a944e2d66f6cf31bd7d7b96b6d179cb559
Soikat Rahman <hello.dev.sr@gmail.com>
2021-09-13 19:41:23 +0600

    init

---
+++ src/app.module.ts
@@ -0,0 +1,41 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const isProduction = configService.get('NODE_ENV') === 'production';
+        console.log(isProduction);
+        return {
+          ssl: isProduction,
+          extra: {
+            ssl: isProduction ? { rejectUnauthorized: false } : null,
+            // !Heroku Deployment
+            /*
+            https://devcenter.heroku.com/articles/heroku-postgresql#heroku-postgres-ssl
+            */
+          },
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USER'),
+          database: configService.get('DB_NAME'),
+          password: configService.get('DB_PASSWORD'),
+          synchronize: true, //allow migration
+          // entities: [User],
+          autoLoadEntities: true,
+          // logging: true,
+        };
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
