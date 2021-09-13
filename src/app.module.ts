c05be8793d0432bda433028fa7ff33c7f4be6025
Soikat Rahman <hello.dev.sr@gmail.com>
2021-09-13 19:41:57 +0600

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
